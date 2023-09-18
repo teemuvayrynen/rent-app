cf63e8fd0248d5d3238fec40de26ca87bd3d765b
Juan Fuentes <59918928+JuanFuentes20@users.noreply.github.com>
2023-09-18 16:23:48 +0300

    Added navbar and footer to global page

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link"
+import Navbar from "@/components/Navbar/Navbar"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>UniRent Landing page</h1>
       <Link href="/search">
         Go to Search Page
       </Link>
+      <Footer />
     </>
   )
 }
