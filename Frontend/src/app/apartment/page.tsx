3312f1a633b7c387f1e7f6fc87329ec4f79e8809
Olli Suoniemi <olli.suoniemi@aalto.fi>
2023-09-28 22:01:43 +0300

    added placeholder for single apartment page

---
+++ Frontend/src/app/apartment/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import SingleApartment from "@/components/SingleApartment/SingleApartment"
+
+const Apartment = () => {
+    return(
+        <SingleApartment/>
+    )
+}
+
+export default Apartment
---
+++ Frontend/src/components/SingleApartment/SingleApartment.js
@@ -0,0 +1,16 @@
+"use client"
+
+import './SingleApartment.css'
+
+function SingleApartment() {
+
+  return (
+    <div className='single-apartment-container'>
+      <p>
+        Single apartment page waits implementing...
+      </p>
+    </div>
+  )
+}
+
+export default SingleApartment;
