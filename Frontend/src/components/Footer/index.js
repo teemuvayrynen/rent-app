5d031f0d2133af28e5681615f722458648085322
Olli Suoniemi <olli.suoniemi@aalto.fi>
2023-11-21 15:48:09 +0200

    change footer text

--- Frontend/src/components/Footer/index.js
+++ Frontend/src/components/Footer/index.js
@@ -7,9 +7,7 @@ function Footer() {
         <div className="footer-info">
           <h2>UniRent</h2>
           <p>
-            Life is a journey filled with twists, turns, and unexpected moments. Embrace each day with curiosity, 
-            find beauty in the ordinary, and cherish the people who make your heart smile. In the end, it&apos;s the
-            memories and connections that truly define our existence.
+            Your gateway to effortless living. Redefining rental experience by making renting easy
           </p>
         </div>
       </div>
